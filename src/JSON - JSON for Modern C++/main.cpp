6dd94aaf854594b79ae74615d370bc02d3e688a7
Fran <fyackel@gmail.com>
2017-08-31 17:47:21 -0300

    Pude hacer andar el Makefile, solo resta no tener que pasar hardcodeado el path de la carpeta 'opencv2/core'

--- src/JSON - JSON for Modern C++/main.cpp
+++ src/JSON - JSON for Modern C++/main.cpp
@@ -13,7 +13,7 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
-#include <opencv2/core/cvdef.h>
+#include "/usr/local/opencv/opencv-3.2.0/modules/core/include/opencv2/core/cvdef.h"
 #include <bitset>
 #include <cstdlib>
 #include <cstring>
@@ -28,29 +28,29 @@ using namespace std;
 using jsonlib = nlohmann::json;
 
 
-void WriteJSON(vector<string> imagenes){	
+void WriteJSON(vector<string> imagenes){
 
-	
+
 	jsonlib json;
-	
-	for(int i=0;i<imagenes.size();i++) { 
+
+	for(int i=0;i<imagenes.size();i++) {
 		string value = imagenes[i];
 		int number = i+1;
 		stringstream ss;
 		ss << number;
 		string str = ss.str();
 		string key = "img" + str;
-		
+
 		json[key] = value;
-		
+
 	}
-	
+
 	string json_string;
-	
+
 	json_string = json.dump(4); // o json.dump();
-	
+
 	//cout<<json_string;
-	
+
 	ofstream file("images.json");
 	file << json_string;
 	file.close();
@@ -58,10 +58,10 @@ void WriteJSON(vector<string> imagenes){
 
 jsonlib ReadJSON(string path){
 	jsonlib json;
-	
+
 	std::ifstream i(path);
 	i >> json;
-	
+
 	return json;
 //	string json_string;
 //	json_string = json.dump(4);
@@ -69,20 +69,20 @@ jsonlib ReadJSON(string path){
 }
 
 int GetCluster(){
-	//	srand((unsigned)time(0)); 
+	//	srand((unsigned)time(0));
 	int i;
-	i = (rand()%10)+1; 
+	i = (rand()%10)+1;
 	return i ;
 }
 
 
 int main(int argc, char** argv) {
-	
+
 	//--------------------
 	/* GENERAR JSON */
 	//--------------------
-	
-	//Debería buscar la forma de automatizar esto
+
+	//Deberï¿½a buscar la forma de automatizar esto
 	vector<string> imagenes;
 	imagenes.push_back("images/1.png");
 	imagenes.push_back("images/2.png");
@@ -90,40 +90,40 @@ int main(int argc, char** argv) {
 	imagenes.push_back("images/4.png");
 	imagenes.push_back("images/5.png");
 	imagenes.push_back("images/6.png");
-	
+
 //	WriteJSON(imagenes);
-	
+
 	//--------------------
 	/* LEVANTAR JSON */
 	//--------------------
-	
+
 	jsonlib json;
-	
+
 	json = ReadJSON("images.json");
-	
+
 	//--------------------
 	/* PROBANDO */
 	//--------------------
-	
-	
+
+
 	jsonlib outputjson;
-	
+
 	//ANDA!
 	for (jsonlib::iterator it = json.begin(); it != json.end(); ++it) {
 //		std::cout << it.key() << " : " << it.value() << "\n";
 		Mat img = imread(it.value());
 		imshow("Probando",img);
-		
-		outputjson[it.key()] ={it.value(),GetCluster()};
-		// or 
-		// outputjson[it.key()] =GetCluster();
-		
+
+//		outputjson[it.key()] ={it.value(),GetCluster()};
+		// or
+		 outputjson[it.key()] =GetCluster();
+
 		waitKey(0);
 	}
-	
+
 	string outjson;
 	outjson = outputjson.dump(4);
 	cout<<outjson;
-	
+
 	return 0;
-} 
+}
